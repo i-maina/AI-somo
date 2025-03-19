37c9f6213c0958fd21060ffc9bc9a74b49e294aa
i-maina <dev.irungu@gmail.com>
2025-03-19 03:58:18 +0300

    create restaurant and menu items

--- RestaurantNameGenerator/main.py
+++ RestaurantNameGenerator/main.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
 st.title("Restaurant Name Generator")
+
+cuisine = st.sidebar.selectbox(
+    "pick a cuisine", ["Italian", "Indian", "Chinese", "Mexican", "American"]
+)
+
+
+def generate_restaurant_name_and_items(cuisine):
+    return {"restaurant_name": "Curry Delight", "menu_items": "samosa, Paneer tikka"}
+
+
+if cuisine:
+    response = generate_restaurant_name_and_items(cuisine)
+    st.header(response["restaurant_name"])
+    menu_items = response["menu_items"].split(",")
+    st.write("*** Menu Items ***")
+
+    for item in menu_items:
+        st.write("-", item)
